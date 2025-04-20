902e317899092e0bb1d59aca415412d16a129db0
Sreejit Sadhukhan <sreejit.s501@gmail.com>
2025-04-20 21:02:08 +0530

    fix: correct import path for Home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home';
+import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Layout from './pages/Layout';
