e918948ef814dd638e0da471755a3e4831fdade4
Sreejit Sadhukhan <sreejit.s501@gmail.com>
2025-04-21 22:08:43 +0530

    fix: simplify Vapi instance initialization by removing unnecessary headers

--- client/src/lib/vapi.sdk.js
+++ client/src/lib/vapi.sdk.js
@@ -1,9 +1,5 @@
 import Vapi from '@vapi-ai/web';
 
-const vapiInstance = new Vapi(import.meta.env.VITE_VAPI_WEB_TOKEN, {
-    headers: {
-      'Authorization': `Bearer ${import.meta.env.VITE_VAPI_WEB_TOKEN}`
-    }
-  });
+const vapi = new Vapi(import.meta.env.VITE_VAPI_WEB_TOKEN);
 
-export default vapiInstance;
+export default vapi;
